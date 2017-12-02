bdcdf1cf10628c67e30a47d9a09721a1cd509b09
jaebradley <jae.b.bradley@gmail.com>
2017-12-02 16:02:35 -0500

    feat(implementation): initial email prop type implementation

---
+++ src/index.test.js
@@ -0,0 +1,96 @@
+import isEmail from 'isemail';
+
+import emailPropType from './index';
+
+describe('Email Prop Type Validation', () => {
+  const propName = 'baejadley';
+  const componentName = 'jaebaebae';
+  const email = 'foobar';
+
+  afterEach(() => {
+    isEmail.validate.mockClear();
+    isEmail.validate.mockRestore();
+  });
+
+  describe('urlPropType', () => {
+    it('should throw an error if prop value is not a valid URL', () => {
+      isEmail.validate = jest.fn().mockImplementation(() => false);
+      const props = {};
+      props[propName] = email;
+
+      expect(() => emailPropType(props, propName, componentName))
+        .toThrow(`Invalid Email Prop Value: ${email} for ${propName} in ${componentName}`);
+      expect(isEmail.validate).toHaveBeenCalledTimes(1);
+      expect(isEmail.validate).toHaveBeenCalledWith(email);
+
+      isEmail.validate.mockClear();
+
+      expect(() => emailPropType(props, propName, componentName))
+        .toThrow(TypeError);
+      expect(isEmail.validate).toHaveBeenCalledTimes(1);
+      expect(isEmail.validate).toHaveBeenCalledWith(email);
+    });
+
+    it('should return null if prop is not defined', () => {
+      const props = {};
+
+      expect(emailPropType(props, propName, componentName)).toBeNull();
+      expect(isEmail.validate).not.toHaveBeenCalled();
+    });
+
+    it('should return null if prop value is a valid email', () => {
+      isEmail.validate = jest.fn().mockImplementation(() => true);
+      const props = {};
+      props[propName] = email;
+
+      expect(emailPropType(props, propName, componentName)).toBeNull();
+      expect(isEmail.validate).toHaveBeenCalledTimes(1);
+      expect(isEmail.validate).toHaveBeenCalledWith(email);
+    });
+  });
+
+  describe('requiredEmailPropType', () => {
+    it('should throw an error if prop value is not a valid email', () => {
+      isEmail.validate = jest.fn().mockImplementation(() => false);
+      const props = {};
+      props[propName] = email;
+
+      expect(() => emailPropType.isRequired(props, propName, componentName))
+        .toThrow(`Invalid Email Prop Value: ${email} for ${propName} in ${componentName}`);
+      expect(isEmail.validate).toHaveBeenCalledTimes(1);
+      expect(isEmail.validate).toHaveBeenCalledWith(email);
+
+      isEmail.validate.mockClear();
+
+      expect(() => emailPropType.isRequired(props, propName, componentName))
+        .toThrow(TypeError);
+      expect(isEmail.validate).toHaveBeenCalledTimes(1);
+      expect(isEmail.validate).toHaveBeenCalledWith(email);
+    });
+
+    it('should throw an error if prop is not defined', () => {
+      const props = {};
+      expect(() => emailPropType.isRequired(props, propName, componentName))
+        .toThrow(`Invalid Email Prop Value: undefined for ${propName} in ${componentName}`);
+      expect(isEmail.validate).toHaveBeenCalledTimes(1);
+      expect(isEmail.validate).toHaveBeenCalledWith(undefined);
+
+      isEmail.validate.mockClear();
+
+      expect(() => emailPropType.isRequired(props, propName, componentName))
+        .toThrow(TypeError);
+      expect(isEmail.validate).toHaveBeenCalledTimes(1);
+      expect(isEmail.validate).toHaveBeenCalledWith(undefined);
+    });
+
+    it('should return null if prop value is a valid email', () => {
+      isEmail.validate = jest.fn().mockImplementation(() => true);
+      const props = {};
+      props[propName] = email;
+
+      expect(emailPropType.isRequired(props, propName, componentName)).toBeNull();
+      expect(isEmail.validate).toHaveBeenCalledTimes(1);
+      expect(isEmail.validate).toHaveBeenCalledWith(email);
+    });
+  });
+});
